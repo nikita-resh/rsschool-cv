66c0f7d284f2af7a8635bc5fbd52d159cb37f89f
nikita-resh <nikita.reshetnikov9@gmail.com>
2021-09-20 23:11:29 +0300

    feat: add selfcheck

---
+++ selfcheck.js
@@ -0,0 +1,18 @@
+console.log("Selfcheck. 150/150");
+console.log("+ 10 Валидная вёрстка");
+console.log(
+  "+ 20 Вёрстка семантичесая: aside, header, main, footer, nav, section, time, h1, h2, h3, h5"
+);
+console.log("+ 10 Используются css - стили");
+console.log("+ 10 Горизонтальное центрирование");
+console.log("+ 10 Адаптивная вёрстка 320+");
+console.log("+ 10 Меню-бургер");
+console.log("+ 10 Фото автора");
+console.log("+ 10 ul > li Контакты и навыки");
+console.log("+ 10 Содержание");
+console.log("+ 10 Подсветка кода");
+console.log("+ 10 Проекты");
+console.log("+ 10 Английский");
+console.log("+ 10 Pull Request");
+console.log("+ 10 Видеорезюме");
+console.log("+ 10 Дизайн и качество не хуже, чем в примерах");
